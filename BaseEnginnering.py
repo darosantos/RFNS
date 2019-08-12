0fc0a9691f288cdab739cfb4dd6d8f41acc9f479
Danilo Santos <danilo_santosrs@hotmail.com>
2019-08-12 19:26:59 -0300

    Alteração da classe BaseEnginnering
    Removido alguns procedimentos

--- BaseEnginnering.py
+++ BaseEnginnering.py
@@ -1,18 +1,23 @@
-class BaseEnginnering:
-	def get_subset(X, y, columns: list):
+class BaseEnginnering(object):
+	def __init__(self):
+		pass
+
+	def get_subset(self, X, y, columns):
 		from pandas import DataFrame
-			
-		if not isinstance(X, DataFrame):
-			raise TypeError('Expected value should descend from pandas.core.frame.DataFrame')
-		
+
+		#if not isinstance(self, X, DataFrame):
+			#raise TypeError('Expected value should descend from pandas.core.frame.DataFrame')
+
 		df_tmp = X.copy()
 		df_tmp.insert(loc=(len(df_tmp.columns)), column='target', value=y)
-		
-		df_subset = (df_tmp[columns].copy(), df_tmp['target'].copy())
-		
+
+		#df_subset = (df_tmp[df_tmp.columns in columns], df_tmp['target'])
+		df_subset = (df_tmp.loc[:, columns], df_tmp['target'])
+
 		return df_subset
-		
-	def arrangement_features(features: list,  n_selected: int) -> list:
+
+	def arrangement_features(self, features: list,  n_selected: int) -> list:
 		from itertools import combinations
 		permsList = list(combinations(features, r=n_selected))
+
 		return permsList
