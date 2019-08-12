2d378949ea5eda95f0ca787d35b92be8e8fbdd10
Danilo Santos <danilo_santosrs@hotmail.com>
2019-08-12 20:11:00 -0300

    #d1001
    Validação dos parâmetros de tipos de dados primitivo; Mudança na forma de selecionar um subconjunto do dataset, eliminando as cópias intermediárias pelo acesso direto ao dataframe

--- BaseEnginnering.py
+++ BaseEnginnering.py
@@ -2,18 +2,26 @@ class BaseEnginnering(object):
 	def __init__(self):
 		pass
 
-	def get_subset(self, X, y, columns):
+	def get_subset(self, X, y, columns: list) -> tuple:
 		from pandas import DataFrame
 
-		df_tmp = X.copy()
-		df_tmp.insert(loc=(len(df_tmp.columns)), column='target', value=y)
-
-		df_subset = (df_tmp.loc[:, columns], df_tmp['target'])
+		if not isinstance(X, DataFrame):
+			raise TypeError('Expected value should descend from pandas.core.frame.DataFrame')
+		if not isinstance(y, DataFrame):
+			raise TypeError('Expected value should descend from pandas.core.frame.DataFrame')
+		if type(columns) != list:
+			raise TypeError('Expectd value list in columns')
+			
+		df_subset = (X.loc[:, columns], y.loc[:,:])
 
 		return df_subset
 
 	def arrangement_features(self, features: list,  n_selected: int) -> list:
 		from itertools import combinations
+		
+		if type(n_selected) != int:
+			raise TypeError('Expected value integer in n_selected')
+			
 		permsList = list(combinations(features, r=n_selected))
 
 		return permsList
