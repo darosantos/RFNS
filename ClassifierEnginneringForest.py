2dd6db9a5369ba6187dd434a458e9e900af9f88d
Danilo Santos <danilo_santosrs@hotmail.com>
2019-08-13 02:45:52 -0300

    #d2001
    Adicionado o destrrutor

--- ClassifierEnginneringForest.py
+++ ClassifierEnginneringForest.py
@@ -12,6 +12,19 @@ class ClassifierEnginneringForest(BaseEnginnering):
 		self.max_leaf_nodes=None
 		self.class_weight=None
 		self.presort=False
+		
+	def __del__(self):
+		del self.criterion
+		del self.splitter
+		del self.max_depth
+		del self.min_samples_split
+		del self.min_samples_leaf
+		del self.min_weight_fraction_leaf
+		del self.max_features
+		del self.random_state
+		del self.max_leaf_nodes
+		del self.class_weight
+		del self.presort
 
 	def make_base_estimator(self):
 		from sklearn.tree import DecisionTreeClassifier
