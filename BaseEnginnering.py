1d754ec9012a54b60f78dbbdfd501929cd7ede66
Daro <danilo_santosrs@hotmail.com>
2019-08-15 13:50:14 -0300

    #d40001

--- BaseEnginnering.py
+++ BaseEnginnering.py
@@ -1,28 +1,27 @@
 class BaseEnginnering(object):
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.train_X = []
+        self.train_y = []
+        
+    def __del__(self):
+        del self.train_X
+        del self.train_y
 
-	def get_subset(self, X, y, columns: list) -> tuple:
-		from pandas import DataFrame, Series
+    def get_subset(self, columns: list) -> tuple:
+        if type(columns) != list:
+            raise TypeError('Expectd value list in columns')
+        
+        df_subset_x = self.train_X.loc[:, columns]
+        df_subset_y = self.train_y.loc[df_subset_x.index]
 
-		if not isinstance(X, DataFrame):
-			raise TypeError('Expected value should descend from pandas.core.frame.DataFrame')
-		if not isinstance(y, Series):
-			raise TypeError('Expected value should descend from pandas.core.frame.DataFrame')
-		if type(columns) != list:
-			raise TypeError('Expectd value list in columns')
-		
-		df_subset_x = X.loc[:, columns]
-		df_subset_y = y.loc[df_subset_x.index]
+        return (df_subset_x, df_subset_y)
 
-		return (df_subset_x, df_subset_y)
+    def arrangement_features(self, features: list,  n_selected: int) -> list:
+        from itertools import combinations
+        
+        if type(n_selected) != int:
+            raise TypeError('Expected value integer in n_selected')
+            
+        permsList = list(combinations(features, r=n_selected))
 
-	def arrangement_features(self, features: list,  n_selected: int) -> list:
-		from itertools import combinations
-		
-		if type(n_selected) != int:
-			raise TypeError('Expected value integer in n_selected')
-			
-		permsList = list(combinations(features, r=n_selected))
-
-		return permsList
+        return permsList
