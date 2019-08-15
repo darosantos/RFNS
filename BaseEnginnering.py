ce2e14c33b7ed3624ff8fada0e5b101d010f738e
Daro <danilo_santosrs@hotmail.com>
2019-08-15 14:30:54 -0300

    #d008

--- BaseEnginnering.py
+++ BaseEnginnering.py
@@ -9,13 +9,13 @@ class BaseEnginnering(object):
     def __del__(self):
         del self.train_X
         del self.train_y
-
+        
     def get_subset(self, columns) -> tuple:
         df_subset_x = self.train_X.loc[:, columns]
         df_subset_y = self.train_y.loc[df_subset_x.index]
-
+        
         return (df_subset_x, df_subset_y)
-
+    
     def arrangement_features(self, features: list,  n_selected: int) -> list:
         from itertools import combinations
         
@@ -23,5 +23,5 @@ class BaseEnginnering(object):
             raise TypeError('Expected value integer in n_selected')
             
         permsList = list(combinations(features, r=n_selected))
-
+        
         return permsList
