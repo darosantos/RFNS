95fdbfa2ccdb56bf62aa8e8f40c4fa120b240992
Daro <danilo_santosrs@hotmail.com>
2019-08-30 15:54:02 -0300

    #d300819004

--- grimoire/ConfigurationEnginnering.py
+++ grimoire/ConfigurationEnginnering.py
@@ -4,6 +4,7 @@ class ConfigurationEnginnering:
         self.chunck = 1
         self.prefix_column_predict = 'cls'
         self.enable_preprocessing = False
+        self.save_matrix_prediction = True
         
     def run(self):
         pass
