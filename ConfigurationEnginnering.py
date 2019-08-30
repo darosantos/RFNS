ddaedc67baa3e93bee678153930e05b99a3d6637
Danilo Santos <danilo_santosrs@hotmail.com>
2019-08-30 11:31:11 -0300

    Updating

--- ConfigurationEnginnering.py
+++
@@ -1,9 +0,0 @@
-class ConfigurationEnginnering:
-
-	def __init__(self):
-		self.chunck = 1
-		self.prefix_column_predict = 'cls'
-        self.enable_preprocessing = False
-        
-	def run(self):
-		pass
