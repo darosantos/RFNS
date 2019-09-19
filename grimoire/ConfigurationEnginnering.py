819f3aadcf8791c0d3cdcd25e413413385381814
Danilo Santos <danilo_santosrs@hotmail.com>
2019-09-19 12:58:02 -0300

    #d190819002
    Altera a classe ConfigurationEnginnering   Adiciona o método run_encoder_data para que o usuário possa especificar qual codificador será usado nos dados (X)

--- grimoire/ConfigurationEnginnering.py
+++ grimoire/ConfigurationEnginnering.py
@@ -1,24 +1,31 @@
 from grimoire.LoggerEnginnering import LoggerEnginnering
 
 from time import gmtime, strftime
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, OneHotEncoder
 
 import numpy as np
 
 
 class ConfigurationEnginnering:
 
-    __slots__ = ('chunck', 'enable_preprocessing', 'autoclean',
-                 'str_data_format', 'auto_coded_target',
+    __slots__ = ('chunck', 'autoclean', 'str_data_format',
+                 'preprocessing_enable', 'preprocessing_data',
+                 'preprocessing_target', 'encoder_X', 'encoder_y',
                  'save_matrix_prediction',  'file_name_matrix_prediction',
                  'format_data_predict', 'delimit_data_predict',
                  'start_logging', 'name_file_log', 'drop_old_log', 'logger')
 
     def __init__(self):
         self.chunck = 32
-        self.enable_preprocessing = False
         self.autoclean = False
         self.str_data_format = "%%d-%B-%Y_%H-%M-%S"
-        self.auto_coded_target = False
+
+        # Configuration for prepossing
+        self.preprocessing_enable = False
+        self.preprocessing_data = False
+        self.preprocessing_target = False
+        self.encoder_X = None
+        self.encoder_y = None
 
         # Configuration for save data predict
         self.save_matrix_prediction = True
@@ -53,7 +60,7 @@ class ConfigurationEnginnering:
             self.logger = LoggerEnginnering(log_file=self.name_file_log,
                                             drop_old=self.drop_old_log)
 
-    def run_save_predict(self, data_predict):
+    def run_save_predict(self, data_predict) -> None:
         if self.save_matrix_prediction:
             if self.file_name_matrix_prediction == '':
                 local_time = strftime(self.str_data_format, gmtime())
@@ -63,3 +70,27 @@ class ConfigurationEnginnering:
             np.savetxt(fname=self.file_name_matrix_prediction, X=data_predict,
                        fmt=self.format_data_predict,
                        delimiter=self.delimit_data_predict)
+
+    def run_encoder_data(self, encoder_type, my_encoder=None):
+        if encoder_type == 0:
+            self.encoder_X = LabelEncoder()
+        elif encoder_type == 1:
+            self.encoder_X = OneHotEncoder(categories='auto',
+                                           sparse=False,
+                                           handle_unknown='ignore')
+        elif encoder_type == 2:
+            self.encoder_X = OrdinalEncoder(categories='auto',
+                                            dtype=np.float64)
+        elif encoder_type == 3:
+            self.encoder_X = my_encoder
+        else:
+            raise TypeError("Don't you specified encoder?")
+
+    def run_transformer(self):
+        if self.preprocessing_data & (self.enconder_X is None):
+            self.encoder_X = OneHotEncoder(categories='auto',
+                                           sparse=False,
+                                           handle_unknown='ignore')
+        if self.preprocessing_target & (self.enconder_y is None):
+            self.encoder_y = LabelEncoder()
+            
