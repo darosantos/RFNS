ec96f68cde15bcab8ced1917275492835a5a13a5
Danilo Santos <danilo_santosrs@hotmail.com>
2019-09-19 10:21:05 -0300

    #d190819001
     Altera a classe ConfigurationEnginnering   Adequa a escrita do código para as orientações PEP

--- grimoire/ConfigurationEnginnering.py
+++ grimoire/ConfigurationEnginnering.py
@@ -4,31 +4,34 @@ from time import gmtime, strftime
 
 import numpy as np
 
+
 class ConfigurationEnginnering:
 
-    __slots__ = ('chunck', 'enable_preprocessing', 'autoclean', 'str_data_format',
+    __slots__ = ('chunck', 'enable_preprocessing', 'autoclean',
+                 'str_data_format', 'auto_coded_target',
                  'save_matrix_prediction',  'file_name_matrix_prediction',
                  'format_data_predict', 'delimit_data_predict',
                  'start_logging', 'name_file_log', 'drop_old_log', 'logger')
-    
+
     def __init__(self):
         self.chunck = 32
         self.enable_preprocessing = False
         self.autoclean = False
         self.str_data_format = "%%d-%B-%Y_%H-%M-%S"
-        
+        self.auto_coded_target = False
+
         # Configuration for save data predict
         self.save_matrix_prediction = True
         self.file_name_matrix_prediction = ''
         self.format_data_predict = '%d'
         self.delimit_data_predict = ','
-        
+
         # Configuraton for logging
         self.start_logging = True
         self.name_file_log = ''
         self.drop_old_log = True
         self.logger = LoggerEnginnering()
-                                        
+
     def __del__(self):
         del self.chunck
         del self.enable_preprocessing
@@ -40,21 +43,23 @@ class ConfigurationEnginnering:
         del self.start_logging
         del self.name_file_log
         del self.drop_old_log
-    
+
     def run_logging(self):
         if self.start_logging:
             if self.name_file_log == '':
                 local_time = strftime(self.str_data_format, gmtime())
-                self.name_file_log =  'enginnering_{0}'.format(local_time)
-            
+                self.name_file_log = 'enginnering_{0}'.format(local_time)
+
             self.logger = LoggerEnginnering(log_file=self.name_file_log,
                                             drop_old=self.drop_old_log)
-                                            
+
     def run_save_predict(self, data_predict):
         if self.save_matrix_prediction:
             if self.file_name_matrix_prediction == '':
                 local_time = strftime(self.str_data_format, gmtime())
-                self.file_name_matrix_prediction = 'matrix_prediction_{0}.txt'.format(local_time)
-                
+                self.file_name_matrix_prediction = 'matrix_prediction_{0}.txt'
+                self.file_name_matrix_prediction.format(local_time)
+
             np.savetxt(fname=self.file_name_matrix_prediction, X=data_predict,
-                       fmt=self.format_data_predict, delimiter=self.delimit_data_predict)
+                       fmt=self.format_data_predict,
+                       delimiter=self.delimit_data_predict)
