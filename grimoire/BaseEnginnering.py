03cfc6bf0ff605a82d0745b4c16fab0329b421f9
Danilo Santos <danilo_santosrs@hotmail.com>
2019-10-12 10:37:47 -0300

    #d121019002
    Altera a classe BaseEnginnering    Adiciona os métodos get_transform_predict e get_normalize_predict para serem usados nos dados de X_test quando o is_data_categorical for true.

--- grimoire/BaseEnginnering.py
+++ grimoire/BaseEnginnering.py
@@ -139,6 +139,57 @@ class BaseEnginnering(ConfigurationEnginnering):
                 self.train_X[c] = normal_values[:, i]
             self.normalize_flag = 1
 
+    def get_transform_predict(self, data_encoder_type=1):
+        condition = [self.encoder_enable, 
+                     self.encoder_data,
+                     (self.encoder_flag[0] == 1)]
+        if all(condition):
+            self.run_encoder_data(data_encoder_type)
+            encoder_df = DataFrame(index=self.predict_X.index)
+            for col in self.predict_X.columns:
+                if type(self.predict_X[col][0]) in self.encoder_not_type:
+                    encoder_df.insert(loc=encoder_df.shape[1],
+                                      column=col,
+                                      value=self.predict_X[col])
+                    self.encoder_feature[col] = type(self.predict_X[col][0])
+                else:
+                    df_col = self.predict_X.loc[:, [col]]
+                    # reverse list of unique values
+                    # convertendo para um list para facilitar nos próximos procedimentos
+                    unique_categories = list(df_col[col].unique()[::-1])
+                    self.encoder_feature[col] = unique_categories
+                    df_tmp = self.encoder_X.fit_transform(df_col)
+                    if (len(df_tmp.shape) == 1):
+                        col_name = '{0}_all'.format(col)
+                        encoder_df.insert(loc=encoder_df.shape[1],
+                                          column=col_name,
+                                          value=df_tmp)
+                        self.encoder_categorical_columns.append(col_name)
+                    else:
+                        index_shape = range(df_tmp.shape[1])
+                        for i, c in zip(index_shape, unique_categories):
+                            col_name = '{0}_{1}'.format(col, c)
+                            encoder_df.insert(loc=encoder_df.shape[1],
+                                              column=col_name,
+                                              value=df_tmp[:, i])
+                            self.encoder_categorical_columns.append(col_name)
+            del self.predict_X
+            self.predict_X = encoder_df.copy()
+            del encoder_df
+            self.encoder_flag[0] = 1
+
+    def get_normalize_predict(self, scaler_type=0):
+        if self.normalize_enable & (self.normalize_flag == 0):
+            column_numerical = [col for col in self.train_X
+                                if col not in self.encoder_categorical_columns]
+            df_tmp = self.train_X.loc[:, column_numerical]
+            self.run_scaler_data(scaler_type)
+            normal_values = self.normalize_scaler.fit_transform(df_tmp)
+            index_shape = range(normal_values.shape[1])
+            for i, c in zip(index_shape, column_numerical):
+                self.train_X[c] = normal_values[:, i]
+            self.normalize_flag = 1
+
     def get_preprocessing(self, data_encoder_type=1,
                           target_encoder_type=0, scaler_type=0):
         if self.preprocessing_enable:
